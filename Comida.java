f173e15de31b37a601584f985ecba88dfec0dd82
Jugador <dm.evilart@gmail.com>
2018-02-23 10:16:20 +0100

    Primer commit, clases Persona y Comida implementadas

---
+++ Comida.java
@@ -0,0 +1,76 @@
+
+/**
+ * Crea objetos que simulan comida.
+ *
+ * @author D4s1ns
+ * @version 2018/02/23
+ */
+public class Comida {
+    // El nombre de la comida.
+    private String nombre;
+    // Cantidad de calorias.
+    private int calorias;
+    
+    /* CONSTRUCTORES */
+    /**
+     * Construye objetos comida.
+     * Se asume que los valores pasados son validos.
+     * @param nombre El nombre de la comida.
+     * @param calorias La cantidad de calorias que tiene la comida.
+     */
+    public Comida(String nombre, int calorias) {
+        this.nombre = nombre;
+        this.calorias = calorias;
+    }
+    
+    /* METODOS GETTER */
+    /**
+     * Devuelve el nombre de la comida.
+     * @return Devuelve el nombre de la comida.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Devuelve la cantidad de calorias de la comida.
+     * @return Devuelve la cantidad de calorias de la comida.
+     */
+    public int getcalorias() {
+        return calorias;
+    }
+    
+    /* METODOS SETTER */
+    /**
+     * Modifica el nombre de la comida.
+     * @param nombre El nuevo nombre de la comida.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Modifica las calorias de la comida.
+     * @param calorias La nueva cantidad de calorias de la comida.
+     */
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+    
+    /* METODOS GENERALES */
+    /**
+     * Devuelve una cadena con toda la informacion de la comida.
+     * @return Devuelve una cadena con toda la informacion de la comida.
+     */
+    public String toString() {
+        return "Nombre: " + nombre + "Calorias: " + calorias;
+    }
+    
+    /**
+     * Muestra por terminal de texto la informacion de la comida.
+     */
+    public void mostrar() {
+        System.out.println(this);
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,139 @@
+
+/**
+ * Crea objetos que simulan personas.
+ *
+ * @author d4s1ns
+ * @version 2018/02/23
+ */
+public class Persona {
+    // El nombre de la persona.
+    private String nombre;
+    // Genero de la persona. [Verdadero] para mujer, [falso] para hombre.
+    private boolean sexo;
+    // Peso(kg).
+    private int peso;
+    // Altura(cm)
+    private int altura;
+    // Edad (anos)
+    private int edad;
+    
+    /* CONSTRUCTORES */
+    /**
+     * Construye objetos persona.
+     * Se asume que se pasan valores validos.
+     * @param nombre El nombre de la persona.
+     * @param sexo El genero biologico de la persona.
+     * El valor [Verdadero] indica una mujer.
+     * [Falso] indica un hombre.
+     * @param peso El peso de la persona expresada kilogramos.
+     * @param altura La altura de la persona expresada en centimetros.
+     * @param edad La edad de la persona en anos.
+     */
+    public Persona(String nombre, boolean sexo, int peso, int altura, int edad) {
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.edad = edad;
+    }
+    
+    /* METODOS GETTER */
+    /**
+     * Devuelve el nombre de la persona.
+     * @return Devuelve el nombre de la persona.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Devuelve el genero biologico de la persona. [Verdadero] para mujer, [Falso] para hombre.
+     * @return Devuelve el genero biologico de la persona. [Verdadero] para mujer, [Falso] para hombre.
+     */
+    public boolean getSexo() {
+        return sexo;
+    }
+    
+    /**
+     * Devuelve el peso en kg de la persona.
+     * @return Devuelve el peso en kg de la persona.
+     */
+    public int getPeso() {
+        return peso;
+    }
+    
+    /**
+     * Devuelve la altura en cm de la persona.
+     * @return Devuelve la altura en cm de la persona.
+     */
+    public int getAltura() {
+        return altura;
+    }
+    
+    /**
+     * Devuelve la edad en anos de la persona.
+     * @return Devuelve la edad en anos de la persona.
+     */
+    public int getEdad() {
+        return edad;
+    }
+    
+    /* METODOS SETTER */
+    /**
+     * Modifica el nombre de la persona.
+     * @param nombre El nuevo nombre de la persona.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Modifica el genero biologico de la persona.
+     * @param sexo [Verdadero] para una mujer, [Falso] para un hombre.
+     */
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+    
+    /**
+     * Modifica el peso de la persona.
+     * @param peso El nuevo peso de la persona expresado en kg.
+     */
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    
+    /**
+     * Modifca la altura de la persona.
+     * @param altura La nueva altura de la persona expresada en cm.
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    /**
+     * Modifica la edad de la persona.
+     * @param edad La nueva edad de la persona expresada en anos.
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    /* METODOS GENERALES */
+    /**
+     * Devuelve una cadena con toda la informacion de la persona.
+     * @return Devuelve una cadena con toda la informacion de la persona.
+     */
+    public String toString() {
+        String sexo = (this.sexo)?"Mujer":"Hombre";
+        return "Nombre: " + nombre + "Sexo: " + sexo + "Peso: " + peso + "Altura :" + altura + "Edad :" + edad;
+    }
+    
+    /**
+     * Muestra por terminal de texto la informacion de la persona.
+     */
+    public void mostrar() {
+        System.out.println(this);
+    }
+    
+}
